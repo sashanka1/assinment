1e9cd629df62653fbd5761c2d33aa1f0267703d6
sashanka1 <sashankasekharsamal777@gmail.com>
2022-09-07 15:46:55 +0530

    readme added

--- tericsort/src/components/employee.jsx
+++ tericsort/src/components/employee.jsx
@@ -4,12 +4,12 @@ function Employees({data}){
 return data.map((e)=>(
     <div className="empdiv" key={nanoid()}>
         
-        <p>{e.name}</p>
-        <p>{e.email}</p>
-        <p>{e.phone}</p>
-        <p>{e.dob}</p>
-        <p>{e.gender}</p>
-        <p>{e.hobbies}</p>
+        <p> Name: {e.name}</p>
+        <p>Email: {e.email}</p>
+        <p>Phoneno: {e.phone}</p>
+        <p>Dob: {e.dob}</p>
+        <p>Gender: {e.gender}</p>
+        <p>hobbie: {e.hobbies}</p>
     </div>
 ))
 }
